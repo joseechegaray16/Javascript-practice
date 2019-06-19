bbb1dff7fb74b300c698c74521031003c73498c3
Jose Echegaray <45981828+joseechegaray16@users.noreply.github.com>
2019-06-19 13:09:55 -0400

    Add files via upload

---
+++ Javascript practice one/scripts/main.js
@@ -0,0 +1,18 @@
+/*
+Commenting in JS uses the same syntax as if you were commenting in CSS
+*/
+
+// or if it is on a single line, no line breaks, you can just use two forward slashes 
+
+window.alert("Please click okay to move on");
+
+document.getElementById("text").innerHTML = "These words were created with Javascript!";
+
+document.getElementById("by").innerHTML = "Lines of JS Written By" + " " + "Me";
+
+var month = "June";
+var day = 19;
+var year = 2019;
+var date = month + " " + day + " " + year;
+
+document.getElementById("Date").innerHTML = date;
